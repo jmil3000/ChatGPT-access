3e46ea9290e67591e2bf9b5582aa687480cdb6fe
Jordan Miller <jordanmiller3k@gmail.com>
2023-03-16 13:25:48 -0600

    chatbot
    gpt-3.5-turbo added

---
+++ chatbot.py
@@ -0,0 +1,45 @@
+import os
+import openai
+import gradio as gr 
+
+YOUR_API_KEY = "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"  
+openai.api_key = YOUR_API_KEY
+
+start_sequence = "\AI:"
+restart_sequence = "\Human:"
+
+prompt = "The following is a conversation with an AI assistant: "
+
+def openai_create(prompt):
+    response = openai.Completion.create(
+           model = "gpt-3.5-turbo",
+           prompt = prompt,
+           temperature = 0.9,
+           max_tokens=150, 
+           top_p=1,
+           frequency_penalty=0, 
+           presence_penalty=0.6, 
+           stop=[" Human:", " AI:"]
+       ) 
+    return response.choices[0].text
+
+def chatgpt_clone(input, history):
+    history = history or []
+    s = list(sum(history, ()))
+    s.append(input)
+    inp = ' '.join(s)
+    output = openai_create(inp)
+    history.append((input, output))
+    return history, history 
+
+block = gr.Blocks()
+
+with block:
+    gr.Markdown("""<h1><center>My chatGPT</center></h1>""")
+    chatbot = gr.Chatbot()
+    message = gr.Textbox(placeholder=prompt)
+    state = gr.State()
+    submit = gr.Button("SEND")
+    submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
+    
+block.launch(debug = True)
